e2a20efa6fddfc0ab1ea607aeb55e02df05745ee
Sabina Malajter <malajterovasabina@gmail.com>
2025-04-11 18:24:07 +0200

    "Create user"

--- src/main/java/sk/malajter/task_management_system/implementation/jdbc/repository/UserJdbcRepository.java
+++ src/main/java/sk/malajter/task_management_system/implementation/jdbc/repository/UserJdbcRepository.java
@@ -3,14 +3,21 @@ package sk.malajter.task_management_system.implementation.jdbc.repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import sk.malajter.task_management_system.api.exception.BadRequestException;
 import sk.malajter.task_management_system.api.exception.InternalErrorException;
 import sk.malajter.task_management_system.api.exception.ResourceNotFoundException;
+import sk.malajter.task_management_system.api.request.UserAddRequest;
 import sk.malajter.task_management_system.domain.User;
 import sk.malajter.task_management_system.implementation.jdbc.mapper.UserRowMapper;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
@@ -26,15 +33,42 @@ public class UserJdbcRepository {
 
     private static final String GET_BY_ID;
 
+    private static final String INSERT;
+
     static {
         logger = LoggerFactory.getLogger(UserJdbcRepository.class);
         GET_ALL = "SELECT * FROM user";
         GET_BY_ID = "SELECT * FROM user WHERE id = ?";
+        INSERT = "INSERT INTO user(id, name, email) VALUES (next value for user_id_seq, ?, ?)";
     }
 
     public UserJdbcRepository(UserRowMapper userRowMapper, JdbcTemplate jdbcTemplate) {
         this.userRowMapper = userRowMapper;
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public long add(UserAddRequest request) {
+        try {
+            final KeyHolder keyHolder = new GeneratedKeyHolder();
+            jdbcTemplate.update(connection -> {
+                final PreparedStatement ps = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, request.getName());
+                ps.setString(2, request.getEmail());
+                return ps;
+            }, keyHolder);
+
+            if (keyHolder.getKey() == null) {
+                logger.error("KeyHolder is null.");
+                throw new InternalErrorException("Error while adding user.");
+            }
+
+            return keyHolder.getKey().longValue();
+        } catch (DataIntegrityViolationException e) {
+            throw new BadRequestException("User with email " + request.getEmail() + " already exist.");
+        } catch (DataAccessException e) {
+            logger.error("Error while adding user.", e);
+            throw new InternalErrorException("Error while adding user.");
+        }
     }
 
     public List<User> getAll() {
