18da88ea42df25e45506161a6a0c4a2236e39aa2
Sabina Malajter <malajterovasabina@gmail.com>
2025-05-27 22:43:53 +0200

    "Implement methods - ProjectJpaRepository and ProjectServiceJpaImpl"

--- src/main/java/sk/malajter/task_management_system/implementation/jpa/repository/ProjectJpaRepository.java
+++ src/main/java/sk/malajter/task_management_system/implementation/jpa/repository/ProjectJpaRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sk.malajter.task_management_system.implementation.jpa.entity.ProjectEntity;
 
+import java.util.List;
+
 @Repository
 public interface ProjectJpaRepository extends JpaRepository<ProjectEntity, Long> {
+
+    // @Query("select * from project where user_id = ?1")
+    List<ProjectEntity> findAllByUserId(Long userId);
 }
--- src/main/java/sk/malajter/task_management_system/implementation/jpa/service/ProjectServiceJpaImpl.java
+++ src/main/java/sk/malajter/task_management_system/implementation/jpa/service/ProjectServiceJpaImpl.java
@@ -1,14 +1,23 @@
 package sk.malajter.task_management_system.implementation.jpa.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import sk.malajter.task_management_system.api.ProjectService;
+import sk.malajter.task_management_system.api.UserService;
+import sk.malajter.task_management_system.api.exception.InternalErrorException;
+import sk.malajter.task_management_system.api.exception.ResourceNotFoundException;
 import sk.malajter.task_management_system.api.request.ProjectAddRequest;
 import sk.malajter.task_management_system.api.request.ProjectEditRequest;
 import sk.malajter.task_management_system.domain.Project;
+import sk.malajter.task_management_system.domain.User;
 import sk.malajter.task_management_system.implementation.jpa.entity.ProjectEntity;
+import sk.malajter.task_management_system.implementation.jpa.entity.UserEntity;
 import sk.malajter.task_management_system.implementation.jpa.repository.ProjectJpaRepository;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Service
@@ -16,39 +25,66 @@ import java.util.List;
 public class ProjectServiceJpaImpl implements ProjectService {
 
     private final ProjectJpaRepository repository;
+    private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(ProjectServiceJpaImpl.class);
 
-    public ProjectServiceJpaImpl(ProjectJpaRepository repository) {
+    public ProjectServiceJpaImpl(ProjectJpaRepository repository, UserService userService) {
         this.repository = repository;
+        this.userService = userService;
     }
 
     @Override
     public Project get(long id) {
-        return null;
+        return repository
+                .findById(id).map(this::mapProjectEntityToProject)
+                .orElseThrow(() -> new ResourceNotFoundException("Project with id " + id + " not found"));
     }
 
     @Override
     public List<Project> getAll() {
-        return List.of();
+        return repository.findAll().stream().map(this::mapProjectEntityToProject).toList();
     }
 
     @Override
     public List<Project> getAllByUser(long userId) {
-        return List.of();
+        if (userService.get(userId) != null) {
+            return repository.findAllByUserId(userId).stream().map(this::mapProjectEntityToProject).toList();
+        }
+
+        // This should never happen because of the check above
+        // which throws an exception if the user does not exist
+        return null;
     }
 
     @Override
     public void delete(long id) {
-
+        if (this.get(id) != null) {
+            repository.deleteById(id);
+        }
     }
 
     @Override
     public long add(ProjectAddRequest request) {
-        return 0;
+        final User user = userService.get(request.getUserId());
+        final UserEntity userEntity = new UserEntity(user.getId(), user.getName(), user.getEmail());
+        try {
+            return repository.save(new ProjectEntity(
+                    userEntity,
+                    request.getName(),
+                    request.getDescription(),
+                    OffsetDateTime.now())).getId();
+        } catch (DataAccessException e) {
+            logger.error("Error while adding user", e);
+            throw new InternalErrorException("Error while adding user");
+        }
     }
 
     @Override
     public void edit(long id, ProjectEditRequest request) {
-
+        final ProjectEntity projectEntity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Project with id " + id + " not found"));
+        projectEntity.setName(request.getName());
+        projectEntity.setDescription(request.getDescription());
+        repository.save(projectEntity);
     }
 
     private Project mapProjectEntityToProject(ProjectEntity entity) {
